8d291710fece2866262c289c93f5df9af7a32aca
SOELexicon <craig_wright156@hotmail.com>
2022-08-13 00:49:02 +0100

    Create onChangeApplyPermissions.sh

---
+++ onChangeApplyPermissions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# pass one or more folders as arguments
+while true; do
+  for f in "$@"; do
+    date
+    echo "Checking $f and subfolders"
+    find=$(find "$f" -type f)
+    while read -r f2; do
+      # strip non-alphanumeric from filename for a variable var name
+      v=${f2//[^[:alpha:]]/}
+      r=$(md5sum "$f2")
+      if [ "$r" != "${!v}" ]; then
+        eval "chmod 0777 -R $@"
+        echo "Applied Permissions $@"
+      fi
+      eval "${v}=\$r"
+    done <<< "$find"
+  done
